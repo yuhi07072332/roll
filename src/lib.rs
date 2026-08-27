4463f76d1d17d05c62a102a71d60917a46f58fad
Yuhi0707 <yuhi07072332@gmail.com>
2026-08-27 13:58:11 +0800

    implement basic functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,22 @@
+use std::cmp::{max, min};
 use std::fs::{self, File};
-use std::io::{self, BufRead, Read, Write, stdout};
+use std::io::{self, BufRead, Write};
 use std::path::{Path, PathBuf};
 use std::time::Duration;
 
 use clap::Parser;
 
-use crossterm::event::{self, Event, KeyCode, KeyEvent};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::style::{Color, Stylize};
 
-use crate::tui::Terminal;
+use crate::tui::{RenderConfig, Terminal};
 
 mod tui;
 
 #[derive(Parser, Debug)]
 #[command(name = "roll")]
 #[command(version = "0.1.0")]
-struct Config {
+struct Args {
     #[arg(short = 'F', long)]
     quit_if_one_screen: bool,
 
@@ -52,21 +53,44 @@ impl ContentView {
     }
 }
 
-fn run_loop(content: &ContentView) -> io::Result<()> {
-    let term = Terminal::init()?;
+pub fn print_error(message: &str) {
+    eprintln!("{} {message}", "error:".with(Color::Red).bold());
+}
+
+pub fn run() -> io::Result<()> {
+    let args = Args::parse();
+    let render_config = tui::RenderConfig {
+        line_numbers: args.line_numbers,
+    };
+
+    let buffer: Buffer = if let Some(file_path) = args.file_path {
+        read_from_file(&file_path)?
+    } else {
+        read_from_stdin()?
+    };
+
+    let mut content_view = ContentView::new(buffer);
+    run_loop(&mut content_view, &render_config)?;
+
+    Ok(())
+}
+
+fn run_loop(content: &mut ContentView, render_config: &RenderConfig) -> io::Result<()> {
+    let mut term = Terminal::init()?;
     let mut stdout = io::stdout();
 
     loop {
-        if event::poll(Duration::from_millis(250))? {
-            if handle_event(event::read()?) {
-                break;
-            }
-        }
-
-        tui::draw_content(&mut stdout, &term, &content)?;
-        tui::draw_status_line(&mut stdout, &term, &content)?;
+        tui::clear_screen(&mut stdout)?;
+        tui::draw_content(&mut stdout, &term, content, render_config)?;
+        tui::draw_status_line(&mut stdout, &term, content)?;
 
         stdout.flush()?;
+
+        if event::poll(Duration::from_millis(250))?
+            && handle_event(event::read()?, content, &mut term)
+        {
+            break;
+        }
     }
 
     Ok(())
@@ -75,31 +99,48 @@ fn run_loop(content: &ContentView) -> io::Result<()> {
 /// # Returns
 /// true if needs to quit
 #[allow(clippy::match_like_matches_macro)]
-fn handle_event(event: Event) -> bool {
-    match event {
-        Event::Key(KeyEvent { code, .. }) => match code {
-            KeyCode::Esc => true,
-            _ => false,
-        },
-        _ => false,
-    }
-}
+fn handle_event(event: Event, content_view: &mut ContentView, terminal: &mut Terminal) -> bool {
+    let file_rows = content_view.buffer.len();
+    let row_offset = &mut content_view.row_offset;
 
-pub fn print_error(message: &str) {
-    eprintln!("{} {message}", "error:".with(Color::Red).bold());
-}
-
-pub fn run() -> io::Result<()> {
-    let config = Config::parse();
-
-    let buffer: Buffer = if let Some(file_path) = config.file_path {
-        read_from_file(&file_path)?
-    } else {
-        read_from_stdin()?
+    let page_down = |row_offset: usize| {
+        min(
+            row_offset + (terminal.height() / 2) as usize,
+            content_view.buffer.len(),
+        )
     };
 
-    let content_view = ContentView::new(buffer);
-    run_loop(&content_view)?;
+    let page_up = |row_offset: usize| row_offset.saturating_sub((terminal.height() / 2) as usize);
 
-    Ok(())
+    match event {
+        Event::Key(KeyEvent {
+            code, modifiers, ..
+        }) => match code {
+            KeyCode::Esc => return true,
+
+            KeyCode::Char('j') | KeyCode::Down | KeyCode::Enter if *row_offset < file_rows => {
+                content_view.row_offset += 1;
+            }
+            KeyCode::Char('k') | KeyCode::Up if *row_offset > 0 => {
+                content_view.row_offset -= 1;
+            }
+
+            KeyCode::Char('d') if modifiers == KeyModifiers::CONTROL => {
+                *row_offset = page_down(*row_offset)
+            }
+            KeyCode::PageDown => *row_offset = page_down(*row_offset),
+            KeyCode::Char('u') if modifiers == KeyModifiers::CONTROL => {
+                *row_offset = page_up(*row_offset)
+            }
+            KeyCode::PageUp => *row_offset = page_up(*row_offset),
+
+            _ => (),
+        },
+        Event::Resize(width, height) => {
+            terminal.resize(width, height);
+        }
+        _ => (),
+    }
+
+    false
 }
--- src/tui.rs
+++ src/tui.rs
@@ -1,11 +1,13 @@
+use std::cmp::min;
+use std::io::{self, Stdout};
+use std::panic;
+
+use crossterm::terminal::{DisableLineWrap, EnableLineWrap};
 use crossterm::{
     QueueableCommand, cursor, execute,
     style::{self, Attribute, Print},
     terminal::{self, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
 };
-
-use std::io::{self, Stdout, Write};
-use std::cmp::{min, max};
 
 use super::ContentView;
 
@@ -15,13 +17,15 @@ pub struct Terminal {
     height: u16,
 }
 
+pub struct RenderConfig {
+    pub line_numbers: bool,
+}
+
 impl Terminal {
     pub fn init() -> io::Result<Terminal> {
         let (width, height) = terminal::size()?;
-
-        terminal::enable_raw_mode()?;
-
-        execute!(io::stdout(), EnterAlternateScreen, Clear(ClearType::All))?;
+        
+        initialize_terminal()?;
 
         Ok(Terminal { width, height })
     }
@@ -32,35 +36,77 @@ impl Terminal {
     pub fn height(&self) -> u16 {
         self.height
     }
-
-    pub fn restore(&self) -> io::Result<()> {
-        terminal::disable_raw_mode()?;
-        execute!(io::stdout(), LeaveAlternateScreen)?;
-
-        Ok(())
+    pub fn resize(&mut self, width: u16, height: u16) {
+        self.width = width;
+        self.height = height;
     }
 }
 
 impl Drop for Terminal {
     fn drop(&mut self) {
-        let _ = self.restore();
+        let _ = restore_terminal();
     }
+}
+
+fn set_panic_hook() {
+    let default_hook = panic::take_hook();
+
+    panic::set_hook(Box::new(move |panic_info| {
+        let _ = restore_terminal();
+        default_hook(panic_info);
+    }));
+}
+
+fn initialize_terminal() -> io::Result<()> {
+    terminal::enable_raw_mode()?;
+    execute!(
+        io::stdout(),
+        EnterAlternateScreen,
+        DisableLineWrap,
+        Clear(ClearType::All),
+    )?;
+
+    set_panic_hook();
+    Ok(())
+}
+
+fn restore_terminal() -> io::Result<()> {
+    terminal::disable_raw_mode()?;
+    execute!(
+        io::stdout(),
+        LeaveAlternateScreen,
+        EnableLineWrap
+    )?;
+
+    Ok(())
+}
+
+pub fn clear_screen(stdout: &mut Stdout) -> io::Result<()> {
+    stdout.queue(Clear(ClearType::All))?;
+    Ok(())
 }
 
 pub fn draw_content(
     stdout: &mut Stdout,
     terminal: &Terminal,
     content_view: &ContentView,
+    render_config: &RenderConfig,
 ) -> io::Result<()> {
     let (buffer, row_offset) = (&content_view.buffer, content_view.row_offset);
     let file_rows = content_view.buffer.len();
 
     stdout.queue(cursor::MoveTo(0, 0))?;
-    for row in 0..min(terminal.height() as usize, file_rows) {
-        let file_row: usize = row + row_offset;
-        stdout
-            .queue(Print(&buffer[file_row]))?
-            .queue(cursor::MoveToNextLine(1))?;
+    for (row_number, row) in buffer
+        .iter()
+        .enumerate()
+        .take(min(row_offset + terminal.height() as usize - 1, file_rows))
+        .skip(row_offset)
+    {
+        if render_config.line_numbers {
+            stdout.queue(Print(format!("{:>3} ", row_number + 1)))?;
+        }
+
+        stdout.queue(Print(row))?.queue(cursor::MoveToNextLine(1))?;
     }
 
     Ok(())
@@ -73,13 +119,20 @@ pub fn draw_status_line(
 ) -> io::Result<()> {
     let file_rows = content_view.buffer.len();
     let current_row = content_view.row_offset;
+    let percentage = ((current_row as f64 / file_rows as f64) * 100.0) as usize;
 
     stdout
         .queue(cursor::MoveTo(0, terminal.height() - 1))?
         .queue(style::SetAttribute(Attribute::Reverse))?
+        .queue(Print(format!("{: <1$}", "", terminal.width() as usize - 1)))?
+        .queue(cursor::MoveToColumn(0))?
         .queue(Print("Esc to quit"))?
-        .queue(cursor::MoveToColumn(terminal.width() - 8))?
-        .queue(Print(format!("{:>3}:{:>3}", current_row, file_rows)))?;
+        .queue(cursor::MoveToColumn(terminal.width() - 15))?
+        .queue(Print(format!(
+            "{:>3}:{:>3} ({:>3}%)",
+            current_row + 1, file_rows, percentage
+        )))?
+        .queue(style::SetAttribute(Attribute::Reset))?;
 
     Ok(())
 }
